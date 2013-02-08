c61550d67f5d7ad0194d177c53508ae95e1a9963
thois <niko.lindvall@helsinki.fi>
2013-02-08 15:56:02 -0800

    part1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,8 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create movie
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +23,13 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  list = rating_list.split ', '
+  list.each do |rating|
+    if uncheck == :un
+      uncheck("ratings_" + rating)
+    else
+      check("ratings_" + rating)
+    end
+  end
+    
 end
